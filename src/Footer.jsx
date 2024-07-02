e198f054404ea169a5423368802ecaca502d251e
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-07-02 18:32:51 +0200

    Style for the navbar and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <div className="text-center">Footer Component</div>;
+  return (
+    <footer className="bg-sky-950 absolute bottom-0 w-full pt-2">
+      <div className="container m-auto text-center text-sky-300">
+        <p className="font-medium pb-0">Event Scheduler</p>
+      </div>
+      ;
+    </footer>
+  );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,24 +2,67 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="text-center">
-      <ul className="flex justify-evenly m-auto py-2">
-        <li className="">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/create-event">Create Event</NavLink>
-        </li>
-        <li>
-          <NavLink to="/event/2">View Event 2</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup">Sign up</NavLink>
-        </li>
-      </ul>
-    </div>
+    <>
+      <nav id="header" className="w-full py-1 bg-slate-50 shadow border-b">
+        <div className="container m-auto flex justify-between mt-0 px-6 py-2">
+          <NavLink to="/">
+            <h1 className="text-2xl text-sky-300 font-bold mr-10">EventS.</h1>
+          </NavLink>
+
+          <div
+            className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1"
+            id="menu"
+          >
+            <nav>
+              <ul className="md:flex items-center justify-between pt-4 md:pt-0 gap-8">
+                <li className="font-bold text-lg hover:text-sky-300">
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="font-bold text-lg hover:text-sky-300">
+                  <NavLink to="/create-event">Create Event</NavLink>
+                </li>
+                <li className="font-bold text-lg hover:text-sky-300">
+                  <NavLink to="/event/2">View Event 2</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div
+            className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4"
+            id="nav-content"
+          >
+            <div className="auth flex items-center w-full md:w-full gap-4">
+              <NavLink to="/login">
+                <button className="bg-sky-300 font-bold px-8 py-2 hover:bg-blue-500 hover:text-white hover:scale-110">
+                  Login
+                </button>
+              </NavLink>
+              <NavLink to="/signup">
+                <button className="bg-transparent border-2 border-sky-300 font-bold px-4 py-2 hover:bg-blue-100">
+                  Sign up
+                </button>
+              </NavLink>
+              <span className="inline-flex bg-sky-300 p-2 relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="#082f49"
+                  className="bi bi-person-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
