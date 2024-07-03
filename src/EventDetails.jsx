1c0892d93d44181a0e246e53625164794a544823
hexzoner <hexzon@gmail.com>
2024-07-03 11:21:39 +0200

    polishing add view events UI

--- src/EventDetails.jsx
+++ src/EventDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Themes from "./Themes";
 
 export default function EventDetails() {
   const { id } = useParams();
@@ -31,17 +32,21 @@ export default function EventDetails() {
   }, [id]);
 
   return (
-    <div className="text-center text-2xl py-8 my-4 bg-base-300 max-w-[700px] m-auto px-6 rounded-lg">
-      <p className="text-center pb-2">Details of the Event ID {id} </p>
-      <div className="text-xl flex flex-col gap-2 my-4">
-        <p className="bg-base-100 py-4 rounded-lg">{form.title}</p>
-        <p className="bg-base-100 py-4 rounded-lg">{form.date}</p>
-        <p className="bg-base-100 py-4 rounded-lg">{form.location}</p>
-        <p className="bg-base-100 py-4 rounded-lg">{form.description}</p>
-
-        <Link to="/">
-          <button className="btn btn-neutral btn-lg w-fit m-auto mt-4">Return To Home</button>
-        </Link>
+    <div className="min-h-[100vh]">
+      <div className="text-2xl py-4 my-10 bg-base-300 max-w-[800px] m-auto px-4 rounded-lg">
+        <div className="flex justify-between">
+          <p className="pb-2 pl-2 pt-4">Event details (ID:{id}) </p>
+          <Themes />
+        </div>
+        <div className="text-xl flex flex-col gap-3 mb-2 mt-4 text-center">
+          <p className="bg-base-100 py-4 rounded-lg">{form.title}</p>
+          <p className="bg-base-100 py-4 rounded-lg">{form.date}</p>
+          <p className="bg-base-100 py-4 rounded-lg">{form.location}</p>
+          <p className="bg-base-100 py-4 rounded-lg min-h-[150px]">{form.description}</p>
+          <Link to="/" className="text-left">
+            <button className="btn btn-neutral  w-fit m-auto mt-3 px-8 text-lg">Back</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
