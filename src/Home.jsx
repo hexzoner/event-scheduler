860b69fbce6a3827b9208fc9fd8723e408c78648
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-07-03 12:17:41 +0200

    Problems with navbar fixed, correct display of hour and year

--- src/Home.jsx
+++ src/Home.jsx
@@ -35,10 +35,6 @@ export default function Home() {
       });
   }, [page]);
 
-  // const date = new Date(2009, 10, 10); // 2009-11-10
-  // const month = date.toLocaleString("default", { month: "long" });
-  // console.log(month);
-
   return (
     <>
       {/* {events.map((event) => {
@@ -68,110 +64,14 @@ export default function Home() {
       <div className="container m-auto py-8">
         <p className="text-center text-xl font-bold">Home Page Component</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 shadow-sm">
-          {/* Starts event 1 */}
-          <div className="flex row items-start shadow-sm" id="event-id">
-            <ul className="text-center p-4 border min-w-48">
-              <li className="text-5xl font-bold">06</li>
-              <li className="text-xl font-bold text-sky-500">April</li>
-
-              <li className="text-md text-slate-500 border-t pt-2 mt-2">
-                2024
-              </li>
-            </ul>
-            <div className="bg-slate-100 p-4  w-full h-full">
-              <p className="text-xl font-bold">Event title</p>
-              <p className="text-md text-slate-500">Some event description</p>
-              <p className="flex align-middle text-md font-semibold mt-4">
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    className="bi bi-geo-alt mr-2 mt-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                  </svg>
-                </span>
-                Schloßbezirk 10, 76131 Karlsruhe
-              </p>
-            </div>
-          </div>
-          {/* Starts event 2 */}
-          <div className="flex row items-start shadow-sm" id="event-id">
-            <ul className="text-center p-4 border min-w-48">
-              <li className="text-5xl font-bold">26</li>
-              <li className="text-xl font-bold text-sky-500">July</li>
-
-              <li className="text-md text-slate-500 border-t pt-2 mt-2">
-                2024
-              </li>
-            </ul>
-            <div className="bg-slate-100 p-4  w-full h-full">
-              <p className="text-xl font-bold">Event title</p>
-              <p className="text-md text-slate-500">Some event description</p>
-              <p className="flex align-middle text-md font-semibold mt-4">
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    className="bi bi-geo-alt mr-2 mt-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                  </svg>
-                </span>
-                Schloßbezirk 10, 76131 Karlsruhe
-              </p>
-            </div>
-          </div>
-          {/* Starts event 3 */}
-          <div className="flex row items-start shadow-sm" id="event-id">
-            <ul className="text-center p-4 border min-w-48">
-              <li className="text-5xl font-bold">15</li>
-              <li className="text-xl font-bold text-sky-500">August</li>
-
-              <li className="text-md text-slate-500 border-t pt-2 mt-2">
-                2024
-              </li>
-            </ul>
-            <div className="bg-slate-100 p-4  w-full h-full">
-              <p className="text-xl font-bold">Event title</p>
-              <p className="text-md text-slate-500">Some event description</p>
-              <p className="flex align-middle text-md font-semibold mt-4">
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    className="bi bi-geo-alt mr-2 mt-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                  </svg>
-                </span>
-                Schloßbezirk 10, 76131 Karlsruhe
-              </p>
-            </div>
-          </div>
-
           {/* -Map with App info test- */}
           {events.map((event) => {
             const eventYear = event.date.slice(0, 4);
             const eventDay = event.date.slice(8, 10);
             const eventMonth = event.date.slice(5, 7);
             const monthName = monthNames[eventMonth - 1];
-            console.log(monthName);
+            console.log(event.date);
 
-            const month = eventMonth.toLocaleString([], {
-              month: "long",
-            });
-
-            // console.log(month);
             return (
               <div className="flex row items-start" key={event.id}>
                 <ul className="text-center p-4 border min-w-48 h-full">
@@ -180,16 +80,18 @@ export default function Home() {
                     {monthName}
                   </li>
 
-                  <li className="text-md text-slate-500 border-t pt-2 mt-2">
-                    {eventYear}
+                  <li className="text-lg font-semibold text-slate-500 border-t pt-2 mt-2">
+                    {event.date.slice(11, 16)}
                   </li>
                 </ul>
-                <div className="bg-slate-100 p-4  w-full h-full">
-                  <p className="text-xl font-bold">{event.title}</p>
+                <div className="bg-slate-100 p-4  w-full h-full relative">
+                  <p className="text-xl font-bold line-clamp-2">
+                    {event.title}
+                  </p>
                   <p className="text-md text-slate-500 line-clamp-2">
                     {event.description}
                   </p>
-                  <p className="flex align-middle text-md font-semibold mt-4">
+                  <p className="flex align-middle text-md font-semibold my-4">
                     <span>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -204,6 +106,9 @@ export default function Home() {
                     </span>
                     {event.location}
                   </p>
+                  <div className="absolute bottom-0 right-0 px-4 py-1 bg-sky-950 text-white text-sm">
+                    {eventYear}
+                  </div>
                 </div>
               </div>
             );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,104 +3,99 @@ const loginStorageKey = "event-schedule-token";
 
 export default function NavBar({ authToken, setAuthToken, setUserID }) {
   return (
-    <>
-      <nav id="header" className="w-full py-1 bg-slate-50 shadow border-b">
-        <div className="container m-auto flex justify-between mt-0 px-6 py-2">
-          <NavLink to="/">
-            <h1 className="text-2xl text-sky-300 font-bold mr-10">EventS.</h1>
-          </NavLink>
+    <nav id="header" className="w-full py-1 bg-slate-50 shadow border-b">
+      <div className="container m-auto flex justify-between mt-0 py-2">
+        <NavLink to="/">
+          <h1 className="text-2xl text-sky-500 font-bold mr-10">EventS.</h1>
+        </NavLink>
 
-          <div
-            className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1"
-            id="menu"
-          >
-            <nav>
-              <ul className="md:flex items-center justify-between pt-4 md:pt-0 gap-8">
-                <li className="font-bold text-lg hover:text-sky-300">
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li className="font-bold text-lg hover:text-sky-300">
-                  <NavLink to="/create-event">Create Event</NavLink>
-                </li>
-                <li className="font-bold text-lg hover:text-sky-300">
-                  <NavLink to="/event/2">View Event 2</NavLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
-
-          <div
-            className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4"
-            id="nav-content"
-          >
-            <div className="auth flex items-center w-full md:w-full gap-4">
-              {authToken.length === 0 ? (
-                <>
-                  <li>
-                    <NavLink
-                      to="/login"
-                      className={({ isActive }) =>
-                        isActive
-                          ? "font-bold text-lg  scale-105 duration-300"
-                          : "scale-100 duration-300"
-                      }
-                    >
-                      <button className="bg-sky-300 font-bold px-8 py-2 hover:bg-blue-500 hover:text-white hover:scale-110">
-                        Login
-                      </button>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/signup"
-                      className={({ isActive }) =>
-                        isActive
-                          ? "font-bold text-lg  scale-105 duration-300"
-                          : "scale-100 duration-300"
-                      }
-                    >
-                      <button className="bg-transparent border-2 border-sky-300 font-bold px-4 py-2 hover:bg-blue-100">
-                        Sign up
-                      </button>
-                    </NavLink>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <button
-                    onClick={(e) => {
-                      setAuthToken("");
-                      setUserID(-1);
-                      localStorage.setItem(
-                        loginStorageKey,
-                        JSON.stringify({ token: "", userID: -1 })
-                      );
-                    }}
-                  >
-                    Logout
-                  </button>
-                  <li>{/* <NavLink to="/">Logout</NavLink> */}</li>
-                </>
-              )}
-              <span className="inline-flex bg-sky-300 p-2 relative hover:rounded-full hover:shadow">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  className="bi bi-person-circle"
-                  viewBox="0 0 16 16"
+        <div
+          className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1"
+          id="menu"
+        >
+          <nav>
+            <ul className="md:flex items-center justify-between pt-4 md:pt-0 gap-8">
+              <li className="font-bold text-lg hover:text-sky-500">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className="font-bold text-lg hover:text-sky-500">
+                <NavLink to="/create-event">Create Event</NavLink>
+              </li>
+              <li className="font-bold text-lg hover:text-sky-500">
+                <NavLink to="/event/2">View Event 2</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div
+          className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4"
+          id="nav-content"
+        >
+          <div className="auth flex items-center w-full md:w-full gap-4">
+            {authToken.length === 0 ? (
+              <>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "font-bold text-lg  scale-105 duration-300"
+                      : "scale-100 duration-300"
+                  }
                 >
-                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                  <path
-                    fillRule="evenodd"
-                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-                  />
-                </svg>
-              </span>
-            </div>
+                  <button className="bg-sky-300 font-bold px-8 py-2 hover:bg-blue-500 hover:text-white hover:scale-110">
+                    Login
+                  </button>
+                </NavLink>
+
+                <NavLink
+                  to="/signup"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "font-bold text-lg  scale-105 duration-300"
+                      : "scale-100 duration-300"
+                  }
+                >
+                  <button className="bg-transparent border-2 border-sky-300 font-bold px-4 py-2 hover:bg-blue-100">
+                    Sign up
+                  </button>
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <button
+                  className="inline-flex bg-sky-300 p-2 relative hover:bg-transparent hover:border-2 hover:border-sky-300  hover:shadow"
+                  onClick={(e) => {
+                    setAuthToken("");
+                    setUserID(-1);
+                    localStorage.setItem(
+                      loginStorageKey,
+                      JSON.stringify({ token: "", userID: -1 })
+                    );
+                  }}
+                >
+                  <span className="inline-flex gap-4 font-bold px-4">
+                    Logout
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      className="bi bi-person-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                      <path
+                        fillRule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                {/* <NavLink to="/">Logout</NavLink> */}
+              </>
+            )}
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
