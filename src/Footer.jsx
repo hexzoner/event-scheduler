3822cf2bae0a717fced68d087745b658ef54d987
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-07-04 16:58:35 +0200

    Problem fixed: hover color in button Log out

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-sky-950 bottom-0 w-full py-2">
-      <div className="container m-auto text-center text-sky-300">
+      <div className="container m-auto text-center text-info">
         <p className="font-medium pb-0">Event Scheduler</p>
       </div>
     </footer>
